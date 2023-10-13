0b22844e40d291113aa43b8da97b96aa0389a97b
Tấn Tài Võ <tantaitanluoc@gmail.com>
2023-10-13 22:20:06 +0700

    adding resources, viewmodel

---
+++ Viewmodel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using QuanLyKho.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKho.ViewModel
+{
+    public class MainViewModel: BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("Hello world");
+        }
+    }
+}

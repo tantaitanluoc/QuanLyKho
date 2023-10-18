4bbb65fba07e4287354f738d5ef18a047d8848ff
Tấn Tài Võ <tantaitanluoc@gmail.com>
2023-10-18 23:22:28 +0700

    UI design

--- Viewmodel/MainViewModel.cs
+++ Viewmodel/MainViewModel.cs
@@ -12,12 +12,19 @@ namespace QuanLyKho.ViewModel
     public class MainViewModel: BaseViewModel
     {
         public ICommand OnLoadedCMD { get; set; }
+        public ICommand UnitCMD { get; set; }
+        public ICommand SupplierCMD { get; set; }
+        public ICommand CustomerCMD { get; set; }
         public MainViewModel()
         {
             OnLoadedCMD = new RelayCommand<object>(p => true, p => {
-               LoginWindow loginWindow = new LoginWindow();
-               loginWindow.ShowDialog();
+               //LoginWindow loginWindow = new LoginWindow();
+               //loginWindow.ShowDialog();
             });
+
+            UnitCMD = new RelayCommand<object>(p => true, p => { MeasuringUnitWindow w = new MeasuringUnitWindow(); w.ShowDialog(); });
+            SupplierCMD = new RelayCommand<object>(p => true, p => { SupplierWindow w = new SupplierWindow(); w.ShowDialog(); });
+            CustomerCMD = new RelayCommand<object>(p => true, p => { CustomerWindow w = new CustomerWindow(); w.ShowDialog(); });
         }
     }
 }

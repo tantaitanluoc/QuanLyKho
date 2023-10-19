e3265d8509eeb9034ef73e78164c860c137c8965
Tấn Tài Võ <tantaitanluoc@gmail.com>
2023-10-20 00:19:55 +0700

    UI design

--- Viewmodel/MainViewModel.cs
+++ Viewmodel/MainViewModel.cs
@@ -15,6 +15,11 @@ namespace QuanLyKho.ViewModel
         public ICommand UnitCMD { get; set; }
         public ICommand SupplierCMD { get; set; }
         public ICommand CustomerCMD { get; set; }
+        public ICommand MaterialCMD { get; set; }
+        public ICommand UserCMD { get; set; }
+        public ICommand ImportCMD { get; set; }
+        public ICommand ExportCMD { get; set; }
+
         public MainViewModel()
         {
             OnLoadedCMD = new RelayCommand<object>(p => true, p => {
@@ -25,6 +30,10 @@ namespace QuanLyKho.ViewModel
             UnitCMD = new RelayCommand<object>(p => true, p => { MeasuringUnitWindow w = new MeasuringUnitWindow(); w.ShowDialog(); });
             SupplierCMD = new RelayCommand<object>(p => true, p => { SupplierWindow w = new SupplierWindow(); w.ShowDialog(); });
             CustomerCMD = new RelayCommand<object>(p => true, p => { CustomerWindow w = new CustomerWindow(); w.ShowDialog(); });
+            MaterialCMD = new RelayCommand<object>(p => true, p => { MaterialWindow w = new MaterialWindow(); w.ShowDialog(); });
+            UserCMD = new RelayCommand<object>(p => true, p => { UserWindow w = new UserWindow(); w.ShowDialog(); });
+            ImportCMD = new RelayCommand<object>(p => true, p => { ImportWindow w = new ImportWindow(); w.ShowDialog(); });
+            ExportCMD = new RelayCommand<object>(p => true, p => { ExportWindow w = new ExportWindow(); w.ShowDialog(); });
         }
     }
 }
